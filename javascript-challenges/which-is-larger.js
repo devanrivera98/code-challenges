45f8c54a0861de1751158660eb7382d4df5bfe57
Devan Rivera <devanrivera98@gmail.com>
2023-06-12 18:48:31 +0000

    completed

--- javascript-challenges/which-is-larger.js
+++ javascript-challenges/which-is-larger.js
@@ -1,11 +1,12 @@
 // Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
 
 function whichIsLarger(f, g) {
-  if (f > g) {
+  if (f() > g()) {
     return 'f';
-  } else if (g > f) {
+  } else if (g() > f()) {
     return 'g';
   } else {
     return 'neither';
   }
 }
+whichIsLarger(() => 5, () => 10);
