ef5a410d9815673cab7ef55928296479aae0e94f
Devan Rivera <devanrivera98@gmail.com>
2023-06-22 23:28:12 +0000

    working solution

--- javascript-challenges/diff-two-array.js
+++ javascript-challenges/diff-two-array.js
@@ -2,9 +2,17 @@ export function diffArray(arr1, arr2) {
   const newArray = [];
   for (let i = 0; i < arr1.length; i++) {
     if (!(arr2.includes(arr1[i]))) {
-      console.log('ok');
+      console.log(arr1.length);
       newArray.push(arr1[i]);
     }
   }
+  for (let i = 0; i < arr2.length; i++) {
+    if (!(arr1.includes(arr2[i]))) {
+      console.log('this is arr2', arr2.length);
+      newArray.push(arr2[i]);
+    }
+  }
+  console.log('this is the new array', newArray);
+  console.log(arr2[3]);
   return newArray;
 }
--- javascript-challenges/diff-two-array.test.js
+++ javascript-challenges/diff-two-array.test.js
@@ -4,7 +4,11 @@ describe('diffArray', () => {
   test('if there is an entry that doesnt match', () => {
     const testArray1 = ['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'];
     const testArray2 = ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub'];
-    const resultArray = diffArray(testArray1, testArray2);
-    expect(resultArray).toEqual(['pink wool']);
+    const resultArray1 = diffArray(testArray1, testArray2);
+    const testArray3 = [1, 'calf', 3, 'piglet'];
+    const testArray4 = [1, 'calf', 3, 4];
+    const resultArray2 = diffArray(testArray3, testArray4);
+    expect(resultArray1).toEqual(['pink wool']);
+    expect(resultArray2).toEqual(['piglet', 4]);
   });
 });
