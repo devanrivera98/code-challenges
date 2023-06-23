33d9398c6d7194ccbdc24bdfc1ebb12802cbed8e
Devan Rivera <devanrivera98@gmail.com>
2023-06-23 20:19:21 +0000

    made another test

--- javascript-challenges/adjacent-elements-products.test.js
+++ javascript-challenges/adjacent-elements-products.test.js
@@ -2,8 +2,11 @@ import { adjacentElements } from './adjacent-element-product';
 
 describe('adjacentElements test', () => {
   test('which pair is the highest', () => {
-    const testVariable = [3, 6, -2, -5, 7, 3];
-    const resultVariable = adjacentElements(testVariable);
-    expect(resultVariable).toBe(21);
+    const testVariable1 = [3, 6, -2, -5, 7, 3];
+    const testVariable2 = [1, 2, 3, 0];
+    const resultVariable1 = adjacentElements(testVariable1);
+    const resultVariable2 = adjacentElements(testVariable2);
+    expect(resultVariable1).toBe(21);
+    expect(resultVariable2).toBe(6);
   });
 });
