c91dfa0680e7ffb1549f1ceae7e916c5481a6e92
Devan Rivera <devanrivera98@gmail.com>
2023-07-01 19:12:25 +0000

    completed seek-and-destroy and made jest

---
+++ javascript-challenges/seek-and-destroy.js
@@ -0,0 +1,17 @@
+export function destroyer(arr, ...args) {
+  console.log(args);
+  return arr.filter(index => !args.includes(index));
+}
+
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+// export function destroyer(arr, ...args) {
+//   const newArray = arr;
+//   for (let i = 0; i <= args.length; i++) {
+//     if (arr.includes(args[i])) {
+//       console.log('this is args[i]', args[i]);
+//       return newArray.filter(number => !arr.includes(number));
+//     }
+//   }
+
+// }
