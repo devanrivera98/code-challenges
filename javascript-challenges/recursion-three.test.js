a2cce12f7f1240b275e01ca77e338c764a26b74d
Devan Rivera <devanrivera98@gmail.com>
2023-07-05 22:55:12 +0000

    Tested edge case for recursion-three

--- javascript-challenges/recursion-three.test.js
+++ javascript-challenges/recursion-three.test.js
@@ -3,6 +3,8 @@ import { replicateRecursion } from './recursion-three';
 describe('testing replicateRecursion', () => {
   it('test if this works', () => {
     const testVariable = replicateRecursion(3, 5);
+    const test2Variable = replicateRecursion(-1, 5);
     expect(testVariable).toEqual([5, 5, 5]);
+    expect(test2Variable).toEqual([]);
   });
 });
