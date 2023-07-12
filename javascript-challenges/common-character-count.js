73a4cb8c68c3a9db94cf80036707e655d2146ab8
Devan Rivera <devanrivera98@gmail.com>
2023-07-12 21:43:08 +0000

    Completed common characters

--- javascript-challenges/common-character-count.js
+++ javascript-challenges/common-character-count.js
@@ -1,10 +1,12 @@
 export function commonCharacters(s1, s2) {
   let count = 0;
-  console.log(s1.split('a').length - 1);
   for (let i = 0; i < s1.length; i++) {
-    if (s2.includes(s1[i])) {
+    if (s2.indexOf(s1[i]) !== -1) {
+      s2 = s2.replace(s1[i], '');
+      // console.log(s2);
       count++;
     }
   }
+  // console.log(s2);
   return count;
 }
