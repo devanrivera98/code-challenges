3b0a8a3f286c386c8c35e08cef3143dd8c130992
Devan Rivera <devanrivera98@gmail.com>
2023-07-19 22:51:57 +0000

    completed alternating sums

--- javascript-challenges/branch-ten-javascript-problems/alternating-sums.js
+++ javascript-challenges/branch-ten-javascript-problems/alternating-sums.js
@@ -1,3 +1,15 @@
 export function alternatingSums(arr) {
   console.log(arr);
+  let indexOne = 0;
+  let indexTwo = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2) {
+      console.log('even', i);
+      indexTwo += arr[i];
+    } else {
+      indexOne += arr[i];
+    }
+  }
+  const newArray = [indexOne, indexTwo];
+  return newArray;
 }
