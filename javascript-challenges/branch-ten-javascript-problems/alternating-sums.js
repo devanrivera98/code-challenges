a1f633968d97942c4ede59af0a248f3f470cd3d6
Devan Rivera <devanrivera98@gmail.com>
2023-07-20 22:40:20 +0000

    finalize solution

--- javascript-challenges/branch-ten-javascript-problems/alternating-sums.js
+++ javascript-challenges/branch-ten-javascript-problems/alternating-sums.js
@@ -3,11 +3,10 @@ export function alternatingSums(arr) {
   let indexOne = 0;
   let indexTwo = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (i % 2) {
-      console.log('even', i);
-      indexTwo += arr[i];
-    } else {
+    if (i % 2 === 0) {
       indexOne += arr[i];
+    } else {
+      indexTwo += arr[i];
     }
   }
   const newArray = [indexOne, indexTwo];
