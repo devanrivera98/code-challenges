6720d19b0feeb2e0d70f54dae067746ea975feba
Devan Rivera <devanrivera98@gmail.com>
2023-07-31 22:31:41 +0000

    arrayReplace

---
+++ javascript-challenges/branch-twelve-javascript-problems/array-replace.js
@@ -0,0 +1,8 @@
+export function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  }
+  return inputArray;
+}
