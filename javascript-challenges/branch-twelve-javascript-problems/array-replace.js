43169a03dad864413c6dd7a98ca165a87e54f251
Devan Rivera <devanrivera98@gmail.com>
2023-07-31 22:48:43 +0000

    checked for edge case

--- javascript-challenges/branch-twelve-javascript-problems/array-replace.js
+++ javascript-challenges/branch-twelve-javascript-problems/array-replace.js
@@ -6,3 +6,5 @@ export function arrayReplace(inputArray, elemToReplace, substitutionElem) {
   }
   return inputArray;
 }
+
+//    return inputArray.map(i => i == elemToReplace ? substitutionElem : i)
