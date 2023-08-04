10737cc5ac7003267bb0df87cf735bcdc1f14c89
Devan Rivera <devanrivera98@gmail.com>
2023-08-04 23:15:01 +0000

    Completed alphabetic shift

--- javascript-challenges/branch-twelve-javascript-problems/alphabetic-shift.js
+++ javascript-challenges/branch-twelve-javascript-problems/alphabetic-shift.js
@@ -1,5 +1,14 @@
 export function alphabeticShift(inputString) {
-  const numericalPlacement = inputString.charCodeAt(0) + 1;
-  console.log(numericalPlacement);
-  return String.fromCodePoint(numericalPlacement);
+
+  let newOutput = '';
+
+  for (let i = 0; i < inputString.length; i++) {
+    let numericalPlacement = inputString.charCodeAt(i) + 1;
+    if (numericalPlacement > 122) {
+      numericalPlacement = 97; // Unicode value for 'a'
+    }
+    const nextLetter = String.fromCodePoint(numericalPlacement);
+    newOutput = newOutput.concat(nextLetter);
+  }
+  return newOutput;
 }
