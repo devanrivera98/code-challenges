2d03f31ba2647e52bb2278146f6910733a97f1f3
Devan Rivera <devanrivera98@gmail.com>
2023-08-11 22:07:06 +0000

    Completed depositProfits

--- javascript-challenges/branch-twelve-javascript-problems/depositProfit.js
+++ javascript-challenges/branch-twelve-javascript-problems/depositProfit.js
@@ -2,7 +2,8 @@ export default function depositProfit(deposit, rate, threshold) {
   let newtotal = deposit;
   let count = 0;
   while (newtotal < threshold) {
-    newtotal *= rate;
+    newtotal += newtotal * (rate / 100);
+    // console.log('this is the final total', newtotal);
     count++;
   }
   return count;
--- javascript-challenges/branch-twelve-javascript-problems/depositProfit.test.js
+++ javascript-challenges/branch-twelve-javascript-problems/depositProfit.test.js
@@ -1,1 +1,12 @@
 import depositProfit from './depositProfit';
+
+describe('depositProfit', () => {
+  test('if this works', () => {
+    const argument1 = 100;
+    const argument2 = 20;
+    const argument3 = 170;
+    const testVariable1 = depositProfit(argument1, argument2, argument3);
+    expect(testVariable1).toEqual(3);
+
+  });
+});
