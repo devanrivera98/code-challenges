e1f920dd49c048db5ffd17f9c4eefd44186315d9
Devan Rivera <devanrivera98@gmail.com>
2023-08-14 22:33:54 +0000

    Completed arrayMaxConsecutive

--- javascript-challenges/branch-thirteen-javascript-problems/array-max-consecutive-sum.js
+++ javascript-challenges/branch-thirteen-javascript-problems/array-max-consecutive-sum.js
@@ -1,12 +1,11 @@
 export default function arrayMaxConsecutiveSum(inputArray, k) {
   let maximumTotal = 0;
   for (let i = 0; i < inputArray.length - (k - 1); i++) {
-    let count = 2;
+    let count = 0;
     let currentCount = 0;
-    while (k >= count) {
-      currentCount = currentCount + inputArray[i] + inputArray[i + 1];
+    while (k > count) {
+      currentCount += inputArray[i + count];
       count++;
-      console.log(inputArray[i], currentCount);
     }
     if (currentCount > maximumTotal) {
       maximumTotal = currentCount;
