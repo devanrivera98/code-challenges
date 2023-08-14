8e3ca1b72c2a13e91142f393d36bbfde719c7a7f
Devan Rivera <devanrivera98@gmail.com>
2023-08-14 21:38:46 +0000

    Completed first-digits

--- javascript-challenges/branch-thirteen-javascript-problems/first-digits.test.js
+++ javascript-challenges/branch-thirteen-javascript-problems/first-digits.test.js
@@ -4,7 +4,7 @@ describe('firstDigits', () => {
   test('if this works', () => {
     const argument1 = 'q2q-q';
     const testVariable1 = firstDigits(argument1);
-    expect(testVariable1).toEqual(2);
+    expect(testVariable1).toEqual('2');
 
   });
 });
