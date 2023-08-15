2c82adf20d4073a69f3ddde5ff4cf179f94b0445
Devan Rivera <devanrivera98@gmail.com>
2023-08-15 20:28:58 +0000

    Completed digit-degree

--- javascript-challenges/branch-thirteen-javascript-problems/digit-degree.test.js
+++ javascript-challenges/branch-thirteen-javascript-problems/digit-degree.test.js
@@ -2,7 +2,7 @@ import digitDegree from './digit-degree';
 
 describe('digitDegree', () => {
   test('if this works', () => {
-    const argument1 = '91';
+    const argument1 = 91;
     const testVariable1 = digitDegree(argument1);
     expect(testVariable1).toEqual(2);
   });
