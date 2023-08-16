ab08dd1ac5e4d93c2c873adc1e2a3e2c6db741a1
Devan Rivera <devanrivera98@gmail.com>
2023-08-16 21:12:31 +0000

    created setup for bishop-pawn

---
+++ javascript-challenges/branch-thirteen-javascript-problems/bishop-and-pawn.test.js
@@ -0,0 +1,11 @@
+import bishopPawn from './bishop-and-pawn';
+
+describe('differentSymbolsNaive', () => {
+  test('if this works', () => {
+    const argument1 = 'a1';
+    const argument2 = 'c3';
+    const testVariable1 = bishopPawn(argument1, argument2);
+    expect(testVariable1).toEqual(true);
+
+  });
+});
