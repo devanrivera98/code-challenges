aeb4525a158852fbd041e5d957d2e65329a35c6a
Devan Rivera <devanrivera98@gmail.com>
2023-08-16 22:40:18 +0000

    shortened solution to bishop and pawn

--- javascript-challenges/branch-thirteen-javascript-problems/bishop-and-pawn.js
+++ javascript-challenges/branch-thirteen-javascript-problems/bishop-and-pawn.js
@@ -1,26 +1,23 @@
 export default function bishopPawn(bishop, pawn) {
-  const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  const row = ['1', '2', '3', '4', '5', '6', '7', '8'];
-  let bishopColor = '';
-  let pawnColor = '';
   if (bishop[0] === pawn[0]) {
     return false;
   }
-  if ((column.indexOf(bishop[0]) % 2 === 0 && row.indexOf(bishop[1]) % 2 === 0) || (column.indexOf(bishop[0]) % 2 !== 0 && row.indexOf(bishop[1]) % 2 !== 0)) {
-    console.log(column.indexOf(bishop[0]) % 2 === 0);
-    console.log(row.indexOf(bishop[1]) % 2 === 0);
-    bishopColor = 'black';
-  } else {
-    bishopColor = 'white';
+  function calculateColor(piece) {
+    const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const row = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+    const columnIndex = column.indexOf(piece[0]);
+    const rowIndex = row.indexOf(piece[1]);
+
+    if ((columnIndex % 2 === 0 && rowIndex % 2 === 0) || (columnIndex % 2 !== 0 && rowIndex % 2 !== 0)) {
+      return 'black';
+    } else {
+      return 'white';
+    }
   }
-  console.log(bishopColor);
-  if ((column.indexOf(pawn[0]) % 2 === 0 && row.indexOf(pawn[1]) % 2 === 0) || (column.indexOf(pawn[0]) % 2 !== 0 && row.indexOf(pawn[1]) % 2 !== 0)) {
-    console.log(column.indexOf(pawn[0]) % 2 === 0);
-    console.log(row.indexOf(pawn[1]) % 2 === 0);
-    pawnColor = 'black';
-  } else {
-    pawnColor = 'white';
-  }
+
+  const bishopColor = calculateColor(bishop);
+  const pawnColor = calculateColor(pawn);
 
   return bishopColor === pawnColor;
 }
