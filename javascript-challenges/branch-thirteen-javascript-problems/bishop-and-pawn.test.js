2ec26d62413fe6914c88f2c518225255a2a41310
Devan Rivera <devanrivera98@gmail.com>
2023-08-16 21:49:46 +0000

    bishop-pawn solution

--- javascript-challenges/branch-thirteen-javascript-problems/bishop-and-pawn.test.js
+++ javascript-challenges/branch-thirteen-javascript-problems/bishop-and-pawn.test.js
@@ -2,7 +2,7 @@ import bishopPawn from './bishop-and-pawn';
 
 describe('differentSymbolsNaive', () => {
   test('if this works', () => {
-    const argument1 = 'a1';
+    const argument1 = 'a7';
     const argument2 = 'c3';
     const testVariable1 = bishopPawn(argument1, argument2);
     expect(testVariable1).toEqual(true);
