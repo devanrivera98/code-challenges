a6fdaa6c2bcd0ffc428119dcd7d02da9b31c616c
Devan Rivera <devanrivera98@gmail.com>
2023-08-17 22:35:04 +0000

    Completed beautiful string

--- javascript-challenges/branch-thirteen-javascript-problems/beautiful-string.js
+++ javascript-challenges/branch-thirteen-javascript-problems/beautiful-string.js
@@ -5,24 +5,24 @@ export default function beautifulString(inputString) {
   // console.log(inputString[0]);
   const uniqueCharacters = [];
   for (let i = 0; i < inputString.length; i++) {
-    console.log(inputString[i]);
     if (!(uniqueCharacters.includes(inputString[i]))) {
       uniqueCharacters.push(inputString[i]);
     }
   }
   uniqueCharacters.sort();
-  console.log(uniqueCharacters);
+  // console.log(uniqueCharacters);
+
   let count = inputString.match(new RegExp(uniqueCharacters[0], 'g')).length;
-  for (let i = 0; i < uniqueCharacters.length; i++) {
+  for (let i = 1; i < uniqueCharacters.length; i++) {
     const newCount = inputString.match(new RegExp(uniqueCharacters[i], 'g')).length;
-    if (newCount <= count) {
-      count = newCount;
-    } else if ((uniqueCharacters[i].charCodeAt() + 1) !== (uniqueCharacters[i - 1].charCodeAt())) {
+    if ((uniqueCharacters[i].charCodeAt() - 1) !== (uniqueCharacters[i - 1].charCodeAt())) {
       return false;
+    } else if (newCount <= count) {
+      count = newCount;
     } else {
       return false;
     }
-    console.log('this is', uniqueCharacters[i].charCodeAt());
+    // console.log('this is', uniqueCharacters[i].charCodeAt());
   }
   return true;
 }
