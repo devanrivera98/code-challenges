438b95d462ca5a21ac7b0d79f8805dfad9903156
Devan Rivera <devanrivera98@gmail.com>
2023-08-21 22:44:53 +0000

    Completed find-email

--- javascript-challenges/branch-thirteen-javascript-problems/find-email-domain.js
+++ javascript-challenges/branch-thirteen-javascript-problems/find-email-domain.js
@@ -1,13 +1,10 @@
 export default function findEmail(address) {
 
   const newIndex = address.indexOf('@');
-  console.log(newIndex);
   const removeIndex = address.replace('@', '');
   const secondIndex = removeIndex.indexOf('@');
-  console.log(secondIndex);
   if (removeIndex.indexOf('@') > -1) {
     return removeIndex.substring(secondIndex + 1);
   }
-  // console.log(removeIndex, newIndex);
   return address.substring(newIndex + 1);
 }
