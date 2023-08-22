6ee64ba13946fbd5c92237abb8addd2100955e44
Devan Rivera <devanrivera98@gmail.com>
2023-08-22 23:28:40 +0000

    completed build-palindrome

--- javascript-challenges/branch-fourteen-javascript-problems/build-palindrome.js
+++ javascript-challenges/branch-fourteen-javascript-problems/build-palindrome.js
@@ -1,8 +1,13 @@
-export default function buildPalindrome(str) {
-  const reverseString = str.split('').reverse().join('');
-  console.log(reverseString);
-  if (str === reverseString) {
-    return str;
+export default function buildPalindrome(st) {
+  if (isPalindrome(st)) return st;
+  for (let i = 0; i < st.length; i++) {
+    if (isPalindrome(st.slice(i, st.length))) {
+      return st + Array.from(st.slice(0, i)).reverse().join('');
+    }
   }
+}
 
+function isPalindrome(string) {
+  console.log(Array.from(string).reverse().join(''));
+  return string === Array.from(string).reverse().join('');
 }
