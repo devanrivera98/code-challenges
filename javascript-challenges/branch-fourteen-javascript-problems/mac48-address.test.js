721b862e6cdd6fa913bfe15edd0ff3a81e5c719d
Devan Rivera <devanrivera98@gmail.com>
2023-08-28 20:09:10 +0000

    tested an edge case for quality assurance

--- javascript-challenges/branch-fourteen-javascript-problems/mac48-address.test.js
+++ javascript-challenges/branch-fourteen-javascript-problems/mac48-address.test.js
@@ -2,8 +2,8 @@ import macAddress from './mac48-address';
 
 describe('macAddress', () => {
   test('if this works', () => {
-    const argument1 = '00-1B-63-84-45-E6';
+    const argument1 = '12.34.56.78.9A.BC';
     const testVariable1 = macAddress(argument1);
-    expect(testVariable1).toEqual(true);
+    expect(testVariable1).toEqual(false);
   });
 });
