bad96c6bab58bce0b4ad43b50e064bebac052c66
Devan Rivera <devanrivera98@gmail.com>
2023-08-31 21:55:42 +0000

    Learning the basics of TypeScript

---
+++ typescript-exercises/basics.js
@@ -0,0 +1,7 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+console.log('Hello');
+console.log('Goodbye');
+console.log(Math.round(7.324546546565));
+const pi = 3.14159;
+console.log(pi);
---
+++ typescript-exercises/basics.ts
@@ -0,0 +1,13 @@
+import { isConstructorDeclaration } from "typescript"
+
+console.log('Hello')
+console.log('Goodbye')
+console.log(Math.round(7.324546546565))
+
+let pi: number = 3.14159;
+
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+// movieTitle = 9;
+// number should not work since it was declared string 
+movieTitle.toUpperCase()
