b883b781ef382c07e9385f0f934ae0fd89ccf4eb
Devan Rivera <devanrivera98@gmail.com>
2023-09-06 21:45:09 +0000

    Completed unions Exercise

--- typescript-exercises/unionsExercise.ts
+++ typescript-exercises/unionsExercise.ts
@@ -2,6 +2,7 @@
 // ******************* PART 1 *******************
 // **********************************************
 // Create a variable called highScore that can be a number OR a boolean
+let highScore: number | boolean;
 
 // **********************************************
 // ******************* PART 2 *******************
@@ -9,12 +10,14 @@
 // create an array called stuff
 // it can be an array of numbers OR an array of strings
 // it cannot be an array of numbers and strings (mixed together)
+const stuff1: number[] | string[] = [];
 
 // **********************************************
 // ******************* PART 3 *******************
 // **********************************************
 // Create a literal type called SkillLevel
 // There are 4 allowed values: "Beginner", "Intermediate", "Advanced", and "Expert"
+type SkillLevel = "Beginner" | 'Intermediate' | 'Advanced' | 'Expert'
 
 // **********************************************
 // ******************* PART 4 *******************
@@ -24,6 +27,7 @@
 // age must be a number
 // sport must be "ski" or "snowboard"
 // level must be a value from the SkillLevel type (from above)
+type SkiSchoolStudent = {name: string, age: number, sport: 'ski' | 'snowboard', level: SkillLevel}
 
 // **********************************************
 // ******************* PART 5 *******************
@@ -32,6 +36,8 @@
 // r should be a number
 // g should be a number
 // b should be a number
+type rgbColor = {r: number, g: number, b: number}
+type hslColor = {h: number, s: number, l: number}
 
 // Define a type to represent an HSL color
 // h should be a number
@@ -39,9 +45,21 @@
 // l should be a number
 
 // Create an array called colors that can hold a mixture of RGB and HSL color types
+let allColors: (rgbColor | hslColor)[] = [];
 
 // **********************************************
 // ******************* PART 6 *******************
 // **********************************************
 // Write a function called greet that accepts a single string OR an array of strings
 // It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+function greet(parameter: string| string[]): string {
+  let returnValue = ''
+  if (typeof parameter === 'string') {
+    returnValue = `Hello, ${parameter}`
+  } else {
+    for (let person of parameter) {
+      returnValue += `Hello, ${person}`
+    }
+  }
+  return returnValue;
+}
