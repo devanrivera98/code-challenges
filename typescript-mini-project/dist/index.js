7982b959278142c196962b126b391f13706fad46
Devan Rivera <devanrivera98@gmail.com>
2023-09-12 22:00:02 +0000

    used type assertions to use value of button

--- typescript-mini-project/dist/index.js
+++ typescript-mini-project/dist/index.js
@@ -1,6 +1,14 @@
 "use strict";
 const btn = document.getElementById('btn');
-console.log(btn);
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', function () {
-    alert('CLICKED');
+// console.log(btn)
+const input = document.getElementById('todoinput');
+// (<HTMLInputElement>input).value
+btn.addEventListener('click', function () {
+    alert(input.value);
+    input.value = "";
 });
+//hypothetical below for type assertion understanding
+// const mystery: unknown = 'Hello World!!'
+// const numChars = (mystery as string).length
+// mystery
+// hypothetical above
--- typescript-mini-project/src/index.ts
+++ typescript-mini-project/src/index.ts
@@ -1,6 +1,18 @@
-const btn = document.getElementById('btn')!;
-console.log(btn)
+const btn = document.getElementById('btn')! as HTMLButtonElement;
+// console.log(btn)
+const input = document.getElementById('todoinput')! as HTMLInputElement;
 
-btn?.addEventListener('click', function() {
-  alert('CLICKED')
+// (<HTMLInputElement>input).value
+
+btn.addEventListener('click', function() {
+  alert(input.value);
+  input.value = "";
 })
+
+
+//hypothetical below for type assertion understanding
+// const mystery: unknown = 'Hello World!!'
+
+// const numChars = (mystery as string).length
+// mystery
+// hypothetical above
