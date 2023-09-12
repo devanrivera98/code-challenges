10a1c371c9463a3cdb8930e0c4d3d82783545c7a
Devan Rivera <devanrivera98@gmail.com>
2023-09-12 21:45:05 +0000

    checking for the HTMLElement

--- typescript-mini-project/dist/index.js
+++ typescript-mini-project/dist/index.js
@@ -1,3 +1,6 @@
-'use strict';
+"use strict";
 const btn = document.getElementById('btn');
 console.log(btn);
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', function () {
+    alert('CLICKED');
+});
--- typescript-mini-project/src/index.ts
+++ typescript-mini-project/src/index.ts
@@ -1,2 +1,6 @@
-const btn = document.getElementById('btn')
+const btn = document.getElementById('btn')!;
 console.log(btn)
+
+btn?.addEventListener('click', function() {
+  alert('CLICKED')
+})
