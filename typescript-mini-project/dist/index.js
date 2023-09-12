2be262ed88383e550ff052d7ee3d1ac60624a458
Devan Rivera <devanrivera98@gmail.com>
2023-09-12 22:57:13 +0000

    created a todo list

--- typescript-mini-project/dist/index.js
+++ typescript-mini-project/dist/index.js
@@ -1,12 +1,27 @@
 "use strict";
 const btn = document.getElementById('btn');
-// console.log(btn)
 const input = document.getElementById('todoinput');
+const form = document.querySelector('form');
+const list = document.getElementById('todolist');
 // (<HTMLInputElement>input).value
-btn.addEventListener('click', function () {
-    alert(input.value);
-    input.value = "";
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("Submitted");
+    const newTodoText = input.value;
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = '';
 });
+//if you want to create your submit function seperately it would look like
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+//   console.log('Submitted');
+// }
+// form.addEventListener('submit', handleSubmit)
 //hypothetical below for type assertion understanding
 // const mystery: unknown = 'Hello World!!'
 // const numChars = (mystery as string).length
--- typescript-mini-project/src/index.ts
+++ typescript-mini-project/src/index.ts
@@ -1,13 +1,31 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement;
-// console.log(btn)
 const input = document.getElementById('todoinput')! as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
 
 // (<HTMLInputElement>input).value
 
-btn.addEventListener('click', function() {
-  alert(input.value);
-  input.value = "";
+form.addEventListener("submit", function(e) {
+  e.preventDefault()
+  console.log("Submitted")
+  const newTodoText = input.value;
+  const newLi = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = "checkbox"
+  newLi.append(newTodoText);
+  newLi.append(checkbox);
+  list.append(newLi);
+  input.value = '';
 })
+
+//if you want to create your submit function seperately it would look like
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+//   console.log('Submitted');
+// }
+// form.addEventListener('submit', handleSubmit)
+
+
 
 
 //hypothetical below for type assertion understanding
