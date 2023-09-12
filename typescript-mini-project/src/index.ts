87dcd987869cc44bf574b09ac9d6835300822405
Devan Rivera <devanrivera98@gmail.com>
2023-09-12 23:51:51 +0000

    cleaned up code on to do typescript

--- typescript-mini-project/src/index.ts
+++ typescript-mini-project/src/index.ts
@@ -1,22 +1,54 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
 const list = document.getElementById('todolist')!;
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
 
-// (<HTMLInputElement>input).value
+
+//load todos from local storage
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos")
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+//save todos to local storage
+function saveTodos() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+}
 
 form.addEventListener("submit", function(e) {
   e.preventDefault()
-  console.log("Submitted")
-  const newTodoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo)
+  todos.push(newTodo);
+
+  saveTodos()
+  input.value = '';
+})
+
+function createTodo(todo: Todo) {
   const newLi = document.createElement('li');
   const checkbox = document.createElement('input');
   checkbox.type = "checkbox"
-  newLi.append(newTodoText);
+  checkbox.checked = todo.completed
+  checkbox.addEventListener('change', function() {
+    saveTodos()
+})
+  newLi.append(todo.text);
   newLi.append(checkbox);
   list.append(newLi);
-  input.value = '';
-})
+}
 
 //if you want to create your submit function seperately it would look like
 // function handleSubmit(e: SubmitEvent) {
