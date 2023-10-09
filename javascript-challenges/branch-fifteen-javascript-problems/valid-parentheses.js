7deef950955953562446a860ab4b4343e3b8c4dc
Devan Rivera <devanrivera98@gmail.com>
2023-10-09 22:08:19 +0000

    fixed

--- javascript-challenges/branch-fifteen-javascript-problems/valid-parentheses.js
+++ javascript-challenges/branch-fifteen-javascript-problems/valid-parentheses.js
@@ -1,11 +1,27 @@
 export default function validParentheses(s) {
-  const regex = /[a-zA-Z0-9]+/g;
-  if (regex.test(s) || s.length % 2 !== 0 || (s[0] === ')' || s[0] === ']' || s[0] === '}')) {
-    return false;
-  }
-  let newString = '';
-  for (let i = 0; i < s.length; i++) {
-    newString += s.slice(i, i + 1);
+  const openMap = new Map([
+    ['(', ')'],
+    ['[', ']'],
+    ['{', '}'],
+  ]);
+  const closeMap = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{'],
+  ]);
+  const stack = [];
 
+  for (let i = 0; i < s.length; i += 1) {
+    if (openMap.get(s[i])) {
+      stack.push(s[i]);
+    } else {
+      const item = stack.pop();
+
+      if (item !== closeMap.get(s[i])) {
+        return false;
+      }
+    }
   }
+
+  return stack.length === 0;
 }
