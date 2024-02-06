2fd84210a98e0cf9ddb7960073ff5b6a93170285
Devan Rivera <devanrivera98@gmail.com>
2024-02-06 23:41:35 +0000

    completed isEmpty

--- javascript-challenges/branch-seventeen-javascript-problems/isEmpty.js
+++ javascript-challenges/branch-seventeen-javascript-problems/isEmpty.js
@@ -1,3 +1,15 @@
 export default function isEmpty(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return true;
+  }
 
+  if (Array.isArray(obj) && obj.length === 0) {
+    return true;
+  }
+
+  if (Object.keys(obj).length === 0) {
+    return true;
+  }
+
+  return false;
 }
--- javascript-challenges/branch-seventeen-javascript-problems/isEmpty.test.js
+++ javascript-challenges/branch-seventeen-javascript-problems/isEmpty.test.js
@@ -2,9 +2,9 @@ import isEmpty from './isEmpty';
 
 describe(('chuck Array'), () => {
   test(('if this works'), () => {
-    const argument1 = {};
+    const argument1 = [];
     const result = isEmpty(argument1);
 
-    expect(result).toEqual(false);
+    expect(result).toEqual(true);
   });
 });
