0212691628a527d7b66c729df2a645963dd021ec
Devan Rivera <devanrivera98@gmail.com>
2024-02-08 00:38:37 +0000

    completed normal cases

--- javascript-challenges/branch-seventeen-javascript-problems/needle-in-hay.test.js
+++ javascript-challenges/branch-seventeen-javascript-problems/needle-in-hay.test.js
@@ -1,2 +1,11 @@
 import needInHay from './needle-in-hay';
 
+describe(('needInHay'), () => {
+  test(('if this works'), () => {
+    const argument1 = 'hegnekeepsygs';
+    const argument2 = 'keep';
+    const result = needInHay(argument1, argument2);
+
+    expect(result).toEqual(5);
+  });
+});
