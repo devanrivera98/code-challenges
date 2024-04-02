239adee0b27e6961ab304db864984897d15c583a
Devan Rivera <devanrivera98@gmail.com>
2024-04-02 03:31:47 +0000

    typescript chuck array finished

--- typescript-challenges/typescript-problems-mar/typescript-chuck-array.ts
+++ typescript-challenges/typescript-problems-mar/typescript-chuck-array.ts
@@ -1,3 +1,11 @@
 export default function chuckArray(arr: number[], size:number ) {
+const newArr: number[][] = []
+if (arr.length === 0) {
+  return newArr
+}
 
+for (let i: number = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, size + i))
+}
+return newArr
 }
