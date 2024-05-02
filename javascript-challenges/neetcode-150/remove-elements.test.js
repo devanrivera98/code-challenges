0a3a55657d1bc967671fbd1b45a3229e7b490c55
Devan Rivera <devanrivera98@gmail.com>
2024-05-02 23:14:55 +0000

    remove-elements done

--- javascript-challenges/neetcode-150/remove-elements.test.js
+++ javascript-challenges/neetcode-150/remove-elements.test.js
@@ -1,10 +1,10 @@
-// import removeElement from "./remove-element";
+import removeElement from "./remove-element";
 
-// describe('removeElements', () => {
-//   test('if this works', () => {
-//     const nums = [0, 1, 2, 2, 3, 0, 4, 2]
-//     const val = 2;
-//     const testVariable1 = removeElement(nums, val)
-//     expect(testVariable1).toEqual(5)
-//   })
-// })
+describe('removeElements', () => {
+  test('if this works', () => {
+    const nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    const val = 2;
+    const testVariable1 = removeElement(nums, val)
+    expect(testVariable1).toEqual(5)
+  })
+})
