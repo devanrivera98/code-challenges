eff54b20005eeca69c144c6fc0f237db08423866
Devan Rivera <devanrivera98@gmail.com>
2024-05-14 03:47:46 +0000

    completed best time

--- javascript-challenges/neetcode-150/best-time.js
+++ javascript-challenges/neetcode-150/best-time.js
@@ -1,3 +1,20 @@
 export default function bestTime(prices) {
 
+  if (prices.length < 2) {
+    return 0; //
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    } else {
+      let potentialProfit = price - minPrice;
+      maxProfit = Math.max(maxProfit, potentialProfit);
+    }
+  }
+
+  return maxProfit > 0 ? minPrice : maxProfit;
 }
