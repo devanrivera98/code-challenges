21de62132b9a0dc7d879f6525770d2bc10414c71
Devan Rivera <devanrivera98@gmail.com>
2024-05-16 23:00:19 +0000

    ransom note completed with hashmap

--- javascript-challenges/neetcode-150/ransom-note.test.js
+++ javascript-challenges/neetcode-150/ransom-note.test.js
@@ -2,9 +2,9 @@ import ransomNote from "./ransom-note";
 
 describe('ransomNote', () => {
   test('if this works', () => {
-    const argument1 = 'aa';
-    const argument2 = 'aab'
+    const argument1 = "fihjjjjei";
+    const argument2 = "hjibagacbhadfaefdjaeaebgi"
     const testVariable1 = ransomNote(argument1, argument2)
-    expect(testVariable1).toEqual(true)
+    expect(testVariable1).toEqual(false)
   })
 })
