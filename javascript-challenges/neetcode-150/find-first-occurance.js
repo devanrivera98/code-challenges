04816096bb5b55febed13433e852477659eb44a4
Devan Rivera <devanrivera98@gmail.com>
2024-05-22 23:18:11 +0000

    completed first Occurance using kmp alg

--- javascript-challenges/neetcode-150/find-first-occurance.js
+++ javascript-challenges/neetcode-150/find-first-occurance.js
@@ -1,15 +1,22 @@
 export default function firstOccurance(haystack, needle) {
-  let currentInstance = ''
   let startIndex = 0;
+  let i = 0;
+  let j = 0;
 
 
-  whil (i > haystack.length) {
-    if (haystack[i] === needle[i]) {
-
+  while (i <= haystack.length) {
+    if (haystack[i] === needle[j]) {
+      i++;
+      j++;
+    } else if (haystack[i] !== needle[j]) {
+      startIndex = startIndex + 1
+      i = startIndex
+      j = 0
     }
 
-    if (haystack[i] !== needle[i]) {
-      startIndex = startIndex + 1
+    if (j === needle.length) {
+      return startIndex
     }
   }
+  return -1
 }
